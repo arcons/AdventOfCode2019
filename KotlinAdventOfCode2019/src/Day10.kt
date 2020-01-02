93fd61f6757335da7cac03c13580e0e366a97503
AlexConsunji <alexander.consunji@slalom.com>
2020-01-02 12:45:34 -0500

    Fixed day10part2

--- KotlinAdventOfCode2019/src/Day10.kt
+++ KotlinAdventOfCode2019/src/Day10.kt
@@ -14,8 +14,10 @@ class Day10(val input: List<String>) {
                 }
             }
         }
-        val asteroidAngles = createVectorMap(asteroidSet);
-        killAstroids(asteroidAngles, asteroidSet);
+        val asteroidData = createVectorMap(asteroidSet);
+        val asteroidAngles = asteroidData.first;
+        val spaceBlasterAngles = asteroidData.second;
+        killAstroids(spaceBlasterAngles, asteroidSet);
 //        println(asteroidSet.toList());
     };
     //
@@ -23,25 +25,28 @@ class Day10(val input: List<String>) {
     // and it's vector of each asteroid around it with
     // check if any of the vectors are divisible with any other value in the set
     // loop through at the end and save the largest number
-    fun createVectorMap (asteroids: Set<Pair<Int, Int>>): Map<Pair<Int, Int>, MutableSet<Double>> {
+    fun createVectorMap (asteroids: Set<Pair<Int, Int>>): Pair<Map<Pair<Int, Int>, MutableSet<Double>>, Map<Double, MutableList<Pair<Int, Int>>>> {
         // create map from set and get the vectors between points
         val asteroidVectorMap = mutableMapOf<Pair<Int, Int>, MutableSet<Double>>();
+        var asteroidAngleMap = mutableMapOf<Double, MutableList<Pair<Int, Int>>>()
         asteroids.forEach{
             val testPosition = it;
             asteroids.forEach{
                 // check that theyre not the same a line
-//                if((testPosition.first != it.first) && (testPosition.second != it.second)) {
-                    // if not the same position then get the x and y between the points
                 if(it != testPosition) {
-                    var magX = testPosition.first - it.first;
-                    var magY = testPosition.second - it.second;
-                    var slope = Math.toDegrees(Math.atan2(magY.toDouble(), magX.toDouble()));
-                    // change the negatives to positives
-                    if(slope < 0) {
-                        slope+=360;
-                    }
+//                    var magX = testPosition.first - it.first;
+//                    var magY = testPosition.second - it.second;
+                    val slope = getAngle(testPosition, it)
                     if (asteroidVectorMap.containsKey(it)) {
                         asteroidVectorMap[it]?.add(slope);
+                        // only do this for the position 22,19 since thats the station
+                        if(testPosition == Pair(22,19)) {
+                            if(asteroidAngleMap.containsKey(slope)) {
+                                asteroidAngleMap[slope]?.add(it);
+                            } else {
+                                asteroidAngleMap.put(slope, mutableListOf<Pair<Int, Int>>(it))
+                            }
+                        }
                     } else {
                         asteroidVectorMap.put(it, mutableSetOf(slope))
                     }
@@ -84,19 +89,74 @@ class Day10(val input: List<String>) {
         }
 
         // Part A
-//        println(bestPair);
-//        println(maxCount);
+        println(bestPair);
+        println(maxCount);
 
-        return asteroidVectorMap;
+        return Pair(asteroidVectorMap, asteroidAngleMap);
     }
 
-    fun killAstroids (asteroidAngles: Map<Pair<Int, Int>, Set<Double>>, allAsteroids: MutableSet<Pair<Int, Int>>) {
-        var numberOfAsteroids = allAsteroids.size;
-        // 22, 19 is the starting point from part A
-        val laserRay = Pair(22,19);
+    fun killAstroids (asteroidAngles: Map<Double, MutableList<Pair<Int, Int>>>, allAsteroids: MutableSet<Pair<Int, Int>>) {
+        var numberOfAsteroids = 282//allAsteroids.size;
+        // 22, 19 is the starting point from part A 282 is the number of asteroids in the line of sight
         // sorted list of the angles
-        println(asteroidAngles[laserRay]?.sorted());
+        val angleList = asteroidAngles.keys?.sorted();
+        // rotate by 90 clockwise for proper quadrants
+        val firstQuadrant = angleList.filter{ it >= (90.0) && it <= 180};
+        var restOfCircle = angleList.filter { !firstQuadrant.contains(it) };
+        var lastAsteroid = Pair(1000, 1000)
+        while (numberOfAsteroids >= 82) {
+            // this will descend from 90
+            firstQuadrant.forEach{
+                if(numberOfAsteroids == 82) {
+                    print("End me fam ")
+                    println(numberOfAsteroids)
+                    println(lastAsteroid);
+                    return
+                };
+                if(asteroidAngles.containsKey(it)) {
+                    val numAtAngle = asteroidAngles[it]?.size ?: 0;
+                    if(numAtAngle > 0) {
+                        lastAsteroid = asteroidAngles[it]?.removeAt(numAtAngle-1) ?: Pair(1000, 1000);
+                        print(lastAsteroid)
+                        print (" at ");
+                        print(it);
+                        println(" has been destroyed")
+                        numberOfAsteroids--;
+                    }
+                }
+            }
+            restOfCircle.forEach{
+                if(numberOfAsteroids == 82)
+                {
+                    print("End me fam ")
+                    println(numberOfAsteroids)
+                    println(lastAsteroid);
+                    return
+                };
+                if(asteroidAngles.containsKey(it)) {
+                    val numAtAngle = asteroidAngles[it]?.size ?: 0;
+                    if(numAtAngle > 0) {
+                        lastAsteroid = asteroidAngles[it]?.removeAt(numAtAngle-1) ?: Pair(1000, 1000);
+                        print(lastAsteroid)
+                        print (" at ");
+                        print(it);
+                        println(" has been destroyed")
+                        numberOfAsteroids--;
+                    }
+                }
+            }
+        }
+//        println(asteroidAngles);
+        // this contains the angles and positions of all the asteroids needing to be blown up
+        // asteroid angle map contains furthest to closest, pop the last item off
+//        println(angleList);
 //        println(allAsteroids);
+    }
+//
+    fun getAngle (testPosition: Pair<Int,Int>, it:Pair<Int, Int>) : Double {
+        var magX = testPosition.first - it.first;
+        var magY = testPosition.second - it.second;
+        return Math.toDegrees(Math.atan2(magY.toDouble(), magX.toDouble()));
     }
 
 }
